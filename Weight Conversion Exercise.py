1c86ddc73b3f465de1f6f9544375cbf6ac01a166
Nikesh24dotk <125327097+Nikesh24dotk@users.noreply.github.com>
2023-02-15 20:38:26 -0500

    Update Weight Conversion Exercise.py

--- Weight Conversion Exercise.py
+++ Weight Conversion Exercise.py
@@ -2,5 +2,5 @@ weight = input("Weight: ")
 unit = input("(K)g or (L)bs: ")
 if(( unit == 'L') or (unit == 'l')):
     print('Weight in Kg: ', (float(((float(weight))/2.2))))
-elif ((unit != 'L') or (unit != 'l')):
-    print('Weight in Kg: ', float(weight))
+elif ((unit == 'K') or (unit == 'k')):
+    print('Weight in Lbs: ', (float(weight)*2.2)
